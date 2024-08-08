0346d5b0654fac3392da1febad4838d17ee2242e
오유진 <omh232323@gmail.com>
2024-08-08 22:31:10 +0900

    [feat] shadcn/ui 세팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Noto_Sans_KR } from "next/font/google";
+import "@/css/globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const notoSans = Noto_Sans_KR({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${notoSans.className} bg-black text-slate-50 max-w-dvw overflow-x-hidden`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
